f7a3f48b7a2f81993381d6f839a5b00f63ed7879
MattMrak <matthewmrakovcic@gmail.com>
2021-02-24 12:40:56 -0500

    commit tree 2a90fac243630d63973501dbcadf786ee84628dd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'appointments/index'
+
+  get 'appointments/new'
+
+  get 'appointments/show'
+
+  get 'appointments/edit'
+
+  get 'patients/index'
+
+  get 'patients/new'
+
+  get 'patients/show'
+
+  get 'patients/edit'
+
+  get 'doctors/index'
+
+  get 'doctors/new'
+
+  get 'doctors/show'
+
+  get 'doctors/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :doctors
   resources :patients
